eee358adae733f5934c36da363bb79b12e4dac28
binshadpb <binshadbinu40@gmail.com>
2020-10-22 22:47:15 +0530

    pending works of online class completed

--- javascript/code.js
+++ javascript/code.js
@@ -1,20 +1,31 @@
-function add(x,y){
-    let sum=x+y
-    console.log("sum is",sum)
+function add(x, y) {
+    let sum = x + y
+    console.log("sum is", sum)
     return sum
 }
 
-let result=add(3,2)
+let result = add(3, 2)
 console.log(result)
 add()
 
-function changecontent(){
-    document.getElementById("p1").innerText="change content"
+//function for change content in the paragraph and style
+
+function changecontent() {
+    document.getElementById("p1").innerText = "change content"
+    document.getElementById("p1").style.color="red"
+    document.getElementById("p1").style.fontSize="50px"
+}
+//function for alert a message as input value on onblur effect
+
+function contentalert() {             
+    let y = document.getElementById("input").value
+    alert(y)
 }
 
-function getchosendata(){
-    
-    
-   let x=document.getElementById("colors").value
-   document.getElementById("color").value=x
+
+//function for dropdown box
+function getchosendata() {
+
+    let x = document.getElementById("colors").value
+    document.getElementById("color").value = x
 }
